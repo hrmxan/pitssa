dc5a405567034efabab6b7fc8f93d48a06369463
HRMxan <hrmxan@gmail.com>
2022-03-24 11:50:28 +0500

    product actions page

--- js/main.js
+++ js/main.js
@@ -74,21 +74,12 @@ const actionsSection = document.getElementById('actionsSection');
 const header = document.getElementById('header');
 const main = document.getElementById('main');
 const footer = document.getElementById('footer');
+const filterBody = document.getElementById('filterBody');
+const productBody = document.getElementById('productBody');
+const productButton = document.getElementById('productButton');
 
-var tooglerActive = true;
-const filterButtonClick = () => {
-    if (tooglerActive) {
-        actionsSection.classList.toggle('closed');
-        header.classList.toggle('blur_back');
-        main.classList.toggle('blur_back');
-        footer.classList.toggle('blur_back');
-        // setTimeout(() => {
-        //     actionsSection.classList.toggle('d-none');
-        // }, 600);
-    }
-}
+// filter tugmalari
 
-filterButton.addEventListener('click', filterButtonClick);
 const filter1 = document.getElementById('filter1');
 const filter2 = document.getElementById('filter2');
 const filter3 = document.getElementById('filter3');
@@ -96,15 +87,88 @@ const filter4 = document.getElementById('filter4');
 const filter5 = document.getElementById('filter5');
 const filter6 = document.getElementById('filter6');
 const filter7 = document.getElementById('filter7');
-filter1.addEventListener('click', filterButtonClick);
-filter2.addEventListener('click', filterButtonClick);
-filter3.addEventListener('click', filterButtonClick);
-filter4.addEventListener('click', filterButtonClick);
-filter5.addEventListener('click', filterButtonClick);
-filter6.addEventListener('click', filterButtonClick);
-filter7.addEventListener('click', filterButtonClick);
 
-// checkbox filterlar uchun 
+// product tugmalar
+const product1 = document.getElementById('pitssaCard1');
+const product2 = document.getElementById('pitssaCard2');
+const product3 = document.getElementById('pitssaCard3');
+const product4 = document.getElementById('pitssaCard4');
+const product5 = document.getElementById('pitssaCard5');
+const product6 = document.getElementById('pitssaCard6');
+const product7 = document.getElementById('pitssaCard7');
+const product8 = document.getElementById('pitssaCard8');
+
+// action  va uning oynalari
+
+const actionOn = () => {
+    actionsSection.classList.remove('closed');
+    header.classList.add('blur_back');
+    main.classList.add('blur_back');
+    footer.classList.add('blur_back');
+}
+const actionOff = () => {
+    actionsSection.classList.add('closed');
+    header.classList.remove('blur_back');
+    main.classList.remove('blur_back');
+    footer.classList.remove('blur_back');
+}
+const filterOn = () => {
+    filterBody.classList.remove('off');
+}
+const filterOff = () => {
+    filterBody.classList.add('off');
+}
+const productOn = () => {
+    productBody.classList.remove('off');
+}
+const productOff = () => {
+    productBody.classList.add('off');
+}
+
+const filterOnToggle = () => {
+    actionOn();
+    filterOn();
+}
+const filterOffToggle = () => {
+    actionOff();
+    filterOff();
+}
+const productOnToggle = () => {
+    actionOn();
+    productOn();
+}
+const productOffToggle = () => {
+    actionOff();
+    productOff();
+}
+
+// action oynalari uchun amallar
+
+    // krestik tugmalar
+filterButton.addEventListener('click', filterOffToggle);
+productButton.addEventListener('click', productOffToggle);
+ // actions uchun tugmalar
+    // filter tugmalar
+filter1.addEventListener('click', filterOnToggle);
+filter2.addEventListener('click', filterOnToggle);
+filter3.addEventListener('click', filterOnToggle);
+filter4.addEventListener('click', filterOnToggle);
+filter5.addEventListener('click', filterOnToggle);
+filter6.addEventListener('click', filterOnToggle);
+filter7.addEventListener('click', filterOnToggle);
+    // product tugmalar
+product1.addEventListener('click', productOnToggle);
+product2.addEventListener('click', productOnToggle);
+product3.addEventListener('click', productOnToggle);
+product4.addEventListener('click', productOnToggle);
+product5.addEventListener('click', productOnToggle);
+product6.addEventListener('click', productOnToggle);
+product7.addEventListener('click', productOnToggle);
+product8.addEventListener('click', productOnToggle);
+
+
+// checkbox filter buttonlaarini o'chirib yoqish
+
 const checkbox_label = document.querySelectorAll('.checkbox_label');
 checkbox_label.forEach(element => {
     element.addEventListener('click', () => {
